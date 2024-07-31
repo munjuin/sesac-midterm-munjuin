fa73b0ab86fe00529067ade6a5d23695d468e291
munjuin <raiawa1212@gmail.com>
2024-07-31 16:05:26 +0900

    -10m

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -2,23 +2,16 @@ window.addEventListener("DOMContentLoaded", () => {
   const getTodos = async () => {
     const response = await fetch("https://jsonplaceholder.typicode.com/todos", {
       method: "GET",
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-      });
-  };
-  request();
+    });
+    const data = await response.json.slice(0, 10);
 
-  const div = document.querySelector(".container");
-  for (let i = 0; i < 11; i++) {
-    div.innerHTML = `
-      <div>
-        <input type="checkbox">
-        <p>${getTodos[i]}</p>
-        <button>X</button>
-    `;
-  }
+    return data;
+  };
+  console.log(getTodos());
+  getTodos();
 });
+
+// const div = document.querySelector(".container");
+// for (let i = 0; i < 11; i++) {
+
+// }
