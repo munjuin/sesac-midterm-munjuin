243541b7a0c6fc2279a063edfbb9ed4201c41429
munjuin <raiawa1212@gmail.com>
2024-07-31 17:07:41 +0900

    -10m

---
+++ backend/models/index.js
@@ -0,0 +1,10 @@
+const Sequelize = require("sequelize"); // sequelize 패키지를 불러옴
+const config = require(__dirname + "/../config/config.json")["development"];
+const db = {};
+
+const sequelize = new Sequelize(
+  config.host,
+  config.username,
+  config.password,
+  config.database
+);
