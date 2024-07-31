5d8cdfc7599f165bef20c934ed2bb29e82a4f776
munjuin <raiawa1212@gmail.com>
2024-07-31 15:07:09 +0900

    40m

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -7,4 +7,18 @@ window.addEventListener("DOMContentLoaded", () => {
 
     return data;
   };
+  getTodos();
 });
+
+const div = document.querySelector(".container");
+for (let i = 0; i < 11; i++) {
+  div.innerHTML = `
+  
+    <div>
+      <input type="checkbox">
+      <p>${getTodos()[i].slice(0, 10)}</p>
+      <button></button>
+  
+    
+  `;
+}
